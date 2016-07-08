23ca9433b32028e2d58a13225008bda8bd920622
Kazuki <Kazuki@DESKTOP-KQMN00P>
2016-07-08 20:54:07 +0900

    プロジェクト ファイルを追加します。

---
+++ CustomListBoxApp/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CustomListBoxApp
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var names = this.ListBox.SelectedItems.Cast<Person>()
+                .Select(x => x.Name)
+                .ToArray();
+            MessageBox.Show(string.Join(", ", names));
+        }
+    }
+}
---
+++ CustomListBoxApp/MainWindowViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomListBoxApp
+{
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public ObservableCollection<Person> People { get; }
+
+        public MainWindowViewModel()
+        {
+            this.People = new ObservableCollection<Person>(Enumerable.Range(1, 100)
+                .Select(x => new Person { Name = $"sample taro {x}" }));
+        }
+    }
+}
